7865d820b3f3a940dd31c7d08a8428ef5779d93d
Your Name <you@example.com>
2021-02-15 08:12:42 +0000

    aa

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -49,10 +49,10 @@ const memberNames = [
 let src = document.getElementById('picture');
 console.log(src);
 
-let ansNumOfMembers = Math.floor(Math.random()*memberNames.length);
-let AnserName = memberNames[ansNumOfMembers];
-let nameEn = AnserName[0];
-let NameJa = AnserName[1];
-let picSrc = '{% static \'Picture/' + nameEn +'.jpeg\' %}';
-console.log(picSrc);
+// let ansNumOfMembers = Math.floor(Math.random()*memberNames.length);
+// let AnserName = memberNames[ansNumOfMembers];
+// let nameEn = AnserName[0];
+// let NameJa = AnserName[1];
+// let picSrc = '{% static \'Picture/' + nameEn +'.jpeg\' %}';
+// console.log(picSrc);
 
