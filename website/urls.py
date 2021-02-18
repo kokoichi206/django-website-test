6c02e3f450633785a3630e449ce909a87f7181be
Kokoichi <tt.300607@gmail.com>
2021-02-19 04:58:32 +0900

    hardQuiz

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from .views import IndexView, AboutView, QuizView
+from .views import QuizEasyView, QuizHardView
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('about/', AboutView.as_view()),
     path('quiz/', QuizView.as_view()),
-    path('quiz/easy', QuizView.as_view()),
-    path('quiz/hard', QuizView.as_view()),
+    path('quiz/easy', QuizEasyView.as_view()),
+    path('quiz/hard', QuizHardView.as_view()),
     # path('contact/', IndexView.as_view()),
 ]
