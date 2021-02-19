62d7fa52cd794829a3edc51dcf91fa22b5562d94
Kokoichi <tt.300607@gmail.com>
2021-02-19 12:43:28 +0900

    login

--- website/urls.py
+++ website/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('about/', AboutView.as_view()),
     path('quiz/', QuizView.as_view()),
     path('quiz/easy', QuizEasyView.as_view()),
+    path('quiz/hard', QuizHardView.as_view()),
     path('login', LoginFirstView.as_view()),
     path('info', AccessFirstView.as_view()),
     # path('contact/', IndexView.as_view()),
