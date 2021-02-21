032404e80a82b36803ca8da98ffe81cf3f609536
Kokoichi <tt.300607@gmail.com>
2021-02-22 03:42:39 +0900

    profile quiz

--- website/static/js/profile.js
+++ website/static/js/profile.js
@@ -45,7 +45,8 @@ const memberNames = [
      ["yumikinao", "弓木 奈於"]      
 ];
 const memberProfiles = {'akimotomanatsu': {'生年月日': '1993年8月20日', '血液型': 'B型', '星座': 'しし座', '身長': '154cm'}, 'ikutaerika': {'生年月日': '1997年1月22日', '血液型': 'A型', '星座': 'みずがめ座', '身長': '160cm'}, 'itoujunna': {'生年月日': '1998年11月30日', '血液型': 'A型', '星座': 'いて座', '身長': '166cm'}, 'itouriria': {'生年月日': '2002年10月8日', '血液型': 'B型', '星座': 'てんびん座', '身長': '154cm'}, 'iwamotorenka': {'生年月日': '2004年2月2日', '血液型': 'B型', '星座': 'みずがめ座', '身長': '159cm'}, 'umezawaminami': {'生年月日': '1999年1月6日', '血液型': 'A型', '星座': 'やぎ座', '身長': '170cm'}, 'oozonomomoko': {'生年月日': '1999年9月13日', '血液型': 'O型', '星座': 'おとめ座', '身長': '156cm'}, 'kitanohinako': {'生年月日': '1996年7月17日', '血液型': 'O型', '星座': 'かに座', '身長': '158cm'}, 'kuboshiori': {'生年月日': '2001年7月14日', '血液型': 'O型', '星座': 'かに座', '身長': '161cm'}, 'saitouasuka': {'生年月日': '1998年8月10日', '血液型': 'O型', '星座': 'しし座', '身長': '158cm'}, 'sakaguchitamami': {'生年月日': '2001年11月10日', '血液型': 'A型', '星座': 'さそり座', '身長': '161cm'}, 'satoukaede': {'生年月日': '1998年3月23日', '血液型': 'A型', '星座': 'おひつじ座', '身長': '161cm'}, 'shinuchimai': {'生年月日': '1992年1月22日', '血液型': 'B型', '星座': 'みずがめ座', '身長': '165cm'}, 'suzukiayane': {'生年月日': '1999年3月5日', '血液型': 'O型', '星座': 'うお座', '身長': '161cm'}, 'takayamakazumi': {'生年月日': '1994年2月8日', '血液型': 'A型', '星座': 'みずがめ座', '身長': '162cm'}, 'teradaranze': {'生年月日': '1998年9月23日', '血液型': '不明', '星座': 'てんびん座', '身長': '155cm'}, 'nakamurareno': {'生年月日': '2001年9月27日', '血液型': '不明', '星座': 'てんびん座', '身長': '167cm'}, 'higuchihina': {'生年月日': '1998年1月31日', '血液型': 'A型', '星座': 'みずがめ座', '身長': '161cm'}, 'hoshinominami': {'生年月日': '1998年2月6日', '血液型': 'B型', '星座': 'みずがめ座', '身長': '155cm'}, 'horimiona': {'生年月日': '1996年10月15日', '血液型': 'O型', '星座': 'てんびん座', '身長': '162cm'}, 'matsumurasayuri': {'生年月日': '1992年8月27日', '血液型': 'B型', '星座': 'おとめ座', '身長': '164cm'}, 'mukaihazuki': {'生年月日': '1999年8月23日', '血液型': 'A型', '星座': 'おとめ座', '身長': '152cm'}, 'yamazakirena': {'生年月日': '1997年5月21日', '血液型': 'B型', '星座': 'ふたご座', '身長': '164cm'}, 'yamashitamizuki': {'生年月日': '1999年7月26日', '血液型': 'O型', '星座': 'しし座', '身長': '160cm'}, 'yoshidaayanochristie': {'生年月日': '1995年9月6日', '血液型': 'A型', '星座': 'おとめ座', '身長': '161cm'}, 'yodayuuki': {'生年月日': '2000年5月5日', '血液型': 'O型', '星座': 'おうし座', '身長': '153cm'}, 'watanabemiria': {'生年月日': '1999年11月1日', '血液型': 'O型', '星座': 'さそり座', '身長': '153cm'}, 'wadamaaya': {'生年月日': '1998年4月23日', '血液型': 'O型', '星座': 'おうし座', '身長': '161cm'}, 'endousakura': {'生年月日': '2001年10月3日', '血液型': '不明', '星座': 'てんびん座', '身長': '164cm'}, 'kakiharuka': {'生年月日': '2001年8月8日', '血液型': 'A型', '星座': 'しし座', '身長': '166cm'}, 'kakehashisayaka': {'生年月日': '2002年11月20日', '血液型': 'B型', '星座': 'さそり座', '身長': '154cm'}, 'kanagawasaya': {'生年月日': '2001年10月31日', '血液型': 'O型', '星座': 'さそり座', '身長': '165cm'}, 'kitagawayuri': {'生年月日': '2001年8月8日', '血液型': '不明', '星座': 'しし座', '身長': '165cm'}, 'kuromiharuka': {'生年月日': '2004年1月19日', '血液型': 'O型', '星座': 'やぎ座', '身長': '162cm'}, 'satourika': {'生年月日': '2001年8月9日', '血液型': 'B型', '星座': 'しし座', '身長': '160cm'}, 'shibatayuna': {'生年月日': '2003年3月3日', '血液型': 'A型', '星座': 'うお座', '身長': '160cm'}, 'seimiyarei': {'生年月日': '2003年8月1日', '血液型': 'O型', '星座': 'しし座', '身長': '164cm'}, 'tamuramayu': {'生年月日': '1999年1月12日', '血液型': 'A型', '星座': 'やぎ座', '身長': '160cm'}, 'tsutsuiayame': {'生年月日': '2004年6月8日', '血液型': 'O型', '星座': 'ふたご座', '身長': '160cm'}, 'hayakawaseira': {'生年月日': '2000年8月24日', '血液型': 'A型', '星座': 'おとめ座', '身長': '164cm'}, 'hayashiruna': {'生年月日': '2003年10月2日', '血液型': 'O型', '星座': 'てんびん座', '身長': '164cm'}, 'matsuomiyu': {'生年月日': '2004年1月3日', '血液型': 'O型', '星座': 'やぎ座', '身長': '167cm'}, 'yakubomio': {'生年月日': '2002年8月14日', '血液型': 'B型', '星座': 'しし座', '身長': '153cm'}, 'yumikinao': {'生年月日': '1999年2月3日', '血液型': 'A型', '星座': 'みずがめ座', '身長': '165cm'}};
-const questions = ['生年月日', '身長'];
+// 上のprofの中から何を質問するか
+let questions = [];
 
 let score = 0;
 let quizIndex = 0;
@@ -54,15 +55,27 @@ let quizIndex = 0;
 const quizLength = 5;
 
 // HTMLのオブジェクトを取ってくる場合には$を入れる
-const $button = document.getElementsByTagName('button');
+const $button = document.getElementsByClassName('btn-primary');
 const buttonLength = $button.length;
 const $question = document.getElementById('js-question');
+const $profileButtons = document.getElementsByName('prof');
 
-let ansNumOfMembers = -1
+let ansNumOfMembers = -1;
 
-let Answer
+console.log($profileButtons[0].style.backgroundColor);
+let Answer;
 // クイズの問題文、選択肢を定義
 const createAnswers = () => {
+     questions = [];
+     // buttonによってquestionに何を採用するか決める。
+     let profloopIndex = 0;
+     while(profloopIndex < 4){
+          if($profileButtons[profloopIndex].style.backgroundColor === 'silver'){
+               questions.push($profileButtons[profloopIndex].textContent);
+          }
+          profloopIndex++;
+     }
+
      let answers = [];
      // questionsの中から質問したい項目をランダムで選ぶ
      questionTipe = questions[Math.floor(Math.random()*questions.length)];
@@ -122,11 +135,30 @@ const clickHandler = (e) => {
      }
 };
 
-// ボタンをクリックしたら正誤判定
+// ボタンをクリックしたら解答
 let handleIndex = 0;
 while (handleIndex < buttonLength) {
      $button[handleIndex].addEventListener('click', (e) => {
           clickHandler(e);
      });
      handleIndex++;
-}
+};
+
+
+
+const changeQuestions = (e) => {
+     // 今回はazureとsilverでONとOFFを切り替えるかー
+     if(e.target.style.backgroundColor === 'silver'){
+          e.target.style.backgroundColor = 'azure'
+     } else {
+          e.target.style.backgroundColor = 'silver';
+     }
+};
+
+let questionIndex = 0;
+while (questionIndex < 4) {
+     $profileButtons[questionIndex].addEventListener('click', (e) => {
+          changeQuestions(e);
+     });
+     questionIndex++;
+};
