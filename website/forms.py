ef8cf2809c895442b7cf7c677f80b9841aeb0b97
kokoichi <tt.300607@gmail.com>
2021-03-01 18:20:57 +0900

    greet

--- website/forms.py
+++ website/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class GreetForm(forms.Form):
     # name = forms.CharField(label='May I have your name?')
-    name = forms.CharField(label='What do you wanna research? kkk ')
+    name = forms.CharField(label='What do you wanna research? k ')
 
