08609e9bf3fe15d52663ff96b578221cbb183267
kokoichi <tt.300607@gmail.com>
2021-03-01 19:27:53 +0900

    greet

--- website/views.py
+++ website/views.py
@@ -99,34 +99,37 @@ class GreetView(FormView):
         URL = URL_top + name
         # URL = 'https://koko-django-website.herokuapp.com/greet/index'
 
-        # response = req.get(URL, timeout=(3.0, 7.5))
-        try:
-            response = requests.get(URL, timeout=5)
-            # print(response.text)
-        except requests.exceptions.ConnectionError as err:
-            return HttpResponse(f'{err}')
-            # pass
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        articles = soup.find_all('div')
-
-        main_blocks = soup.find_all('div', class_='ZINbbc')
-        # print(h2[2])
-        c = 0
-        contens = []
-        for block in main_blocks:
-            title = block.find('h3')
-            if title is not None:
-                content = block.find('div', class_='BNeawe s3v9rd AP7Wnd')
-                if content is not None:
-                    # print(title.text)
-                    # print(content.text)
-                    contents.append([title.text, content.text])
-                    c += 1
-                # print('-' * 100)
-        # print(c)
-        con = ''.join(contents)
-        # return HttpResponse(f'{contents}')
         return HttpResponse(f'{URL}')
+
+        # # response = req.get(URL, timeout=(3.0, 7.5))
+        # try:
+        #     response = requests.get(URL, timeout=5)
+        #     # print(response.text)
+        # except requests.exceptions.ConnectionError as err:
+        #     return HttpResponse(f'{err}')
+        #     # pass
+
+        # soup = BeautifulSoup(response.text, 'html.parser')
+        # articles = soup.find_all('div')
+
+        # main_blocks = soup.find_all('div', class_='ZINbbc')
+        # # print(h2[2])
+        # c = 0
+        # contens = []
+        # for block in main_blocks:
+        #     title = block.find('h3')
+        #     if title is not None:
+        #         content = block.find('div', class_='BNeawe s3v9rd AP7Wnd')
+        #         if content is not None:
+        #             # print(title.text)
+        #             # print(content.text)
+        #             contents.append([title.text, content.text])
+        #             c += 1
+        #         # print('-' * 100)
+        # # print(c)
+        # con = ''.join(contents)
+        # # return HttpResponse(f'{contents}')
+        # return HttpResponse(f'{URL}')
 
 
