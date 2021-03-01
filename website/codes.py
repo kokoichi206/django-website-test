54b13b38c0dd347c75852e6bfda5e8becc9cb7ba
kokoichi <tt.300607@gmail.com>
2021-03-02 01:19:22 +0900

    greet

--- website/codes.py
+++ website/codes.py
@@ -3,5 +3,5 @@ from django import forms
 
 class CodeForm(forms.Form):
     # name = forms.CharField(label='May I have your name?')
-    code = forms.CharField(label='Code here<br＞')
+    code = forms.CharField(label='Code here\n')
 
