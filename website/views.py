626f4d77f254decea5234bb893a221ec955aba54
kokoichi <tt.300607@gmail.com>
2021-03-02 00:05:55 +0900

    greet

--- website/views.py
+++ website/views.py
@@ -119,10 +119,42 @@ class GreetView(FormView):
 
     def ajax_response(self, form):
         name = form.cleaned_data.get('name')
-        ans = form_class.search(name)
-        return HttpResponse(f'{ans}')
+        # ans = form_class.search(name)
+        # return HttpResponse(f'{ans}')
 
 
+        from bs4 import BeautifulSoup
+        import requests
+
+        URL_top = 'https://www.google.com/search?q='
+        # word = '筒井あやめ'
+        URL = URL_top + word
+
+        try:
+            response = requests.get(URL, timeout=5)
+        except requests.exceptions.ConnectionError as err:
+            return HttpResponse(f'error: cannot open the URL')
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        articles = soup.find_all('div')
 
 
+        main_blocks = soup.find_all('div', class_='ZINbbc')
+        contents = []
+        for block in main_blocks:
+            title = block.find('h3')
+            if title is not None:
+                content = block.find('div', class_='BNeawe s3v9rd AP7Wnd')
+                if content is not None:
+                    contents.append([title.text, content.text])
 
+        ans = ''
+        for con in contents:
+            ans += '<br>'.join(con)
+            # ans = ans + '-' * 100 + '\n'
+            ans = ans + '<br>' + '-' * 100 + '<br>'
+
+        if ans:
+            return HttpResponse(f'{ans}')
+        else:
+            return HttpResponse(f'Nothing')
