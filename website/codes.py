aabebaf15fed0b20d9e0a5259f16c762c262ab43
kokoichi <tt.300607@gmail.com>
2021-03-02 01:56:28 +0900

    greet

--- website/codes.py
+++ website/codes.py
@@ -3,5 +3,5 @@ from django import forms
 
 class CodeForm(forms.Form):
     # code = forms.CharField(label='Code here\n')
-    code = forms.CharField(widget=forms.Textarea(attrs={'cols': '80', 'rows': '10'}))
+    code = forms.CharField(widget=forms.Textarea(attrs={'cols': '80', 'rows': '10'}), required=False)
 
