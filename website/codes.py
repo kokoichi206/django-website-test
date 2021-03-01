c32498348d299c9f3abd14f15ccee9cbe74a50d6
kokoichi <tt.300607@gmail.com>
2021-03-02 02:46:34 +0900

    greet

--- website/codes.py
+++ website/codes.py
@@ -3,9 +3,9 @@ from django import forms
 
 class CodeForm(forms.Form):
     # code = forms.CharField(label='Code here\n')
-    code = forms.CharField(label='', widget=forms.Textarea(attrs={'cols': '80', 'rows': '10'}), required=False)
+    code = forms.CharField(label='', widget=forms.Textarea(attrs={'cols': '80', 'rows': '10', 'class': 'prettyprint linenums'}), required=False)
 
-    def __init__(self): 
-        super().__init__(*args, **kwargs)
-        self.fields['code'].widget.attrs["class"] = "prettyprint linenums"
+    # def __init__(self):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['code'].widget.attrs["class"] = "prettyprint linenums"
 
