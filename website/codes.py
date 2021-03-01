bfc8a83acf0c4cf60d1272521e8fbf22c42d287d
kokoichi <tt.300607@gmail.com>
2021-03-02 01:48:45 +0900

    greet

--- website/codes.py
+++ website/codes.py
@@ -3,5 +3,6 @@ from django import forms
 
 class CodeForm(forms.Form):
     # name = forms.CharField(label='May I have your name?')
-    code = forms.CharField(label='Code here\n')
+    # code = forms.CharField(label='Code here\n')
+    code = forms.CharField(label='Code here\n', widget=forms.Textarea(attrs={'cols': '80', 'rows': '10'}))
 
