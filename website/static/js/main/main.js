08e91c0a7943d5609e4d057899252211d2f5c138
kokoichi <tt.300607@gmail.com>
2021-09-01 09:20:48 +0900

    fix: sidebar prob

--- website/static/js/main/main.js
+++ website/static/js/main/main.js
@@ -1,11 +1,11 @@
-$(function() {
-    let target = $("#sidebar").offset().top;
-    $(window).on("scroll", function() {
-        let currentPos = $(window).scrollTop();
-        if(currentPos > target-80) {
-          $("#sidebar").addClass('sticky');
-        } else{
-          $("#sidebar").removeClass('sticky');
-        }
-    });
-});
+// $(function() {
+//     let target = $("#sidebar").offset().top;
+//     $(window).on("scroll", function() {
+//         let currentPos = $(window).scrollTop();
+//         if(currentPos > target-80) {
+//           $("#sidebar").addClass('sticky');
+//         } else{
+//           $("#sidebar").removeClass('sticky');
+//         }
+//     });
+// });
